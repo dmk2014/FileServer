13c86c289618001fb5db6b78d282fc78a5ac3b6c
Darren Keane <darren.m.keane@students.ittralee.ie>
2015-04-02 16:28:28 +0100

    Added unit tests

---
+++ src/main/java/test/FileServerTest.java
@@ -0,0 +1,67 @@
+package test;
+
+import ftp.FileServer;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+public class FileServerTest {
+    FileServer ftpServer;
+
+    @Before
+    public void setUp() throws Exception {
+        ftpServer = new FileServer();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ftpServer = null;
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        String userToLogin = "Michael";
+        boolean result = ftpServer.login(userToLogin);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testLogoff() throws Exception {
+        String username = "John";
+        boolean loginSuccess = ftpServer.login(username);
+        
+        if(loginSuccess)
+        {
+            boolean logoffSuccess = ftpServer.logoff(username);
+            Assert.assertTrue(logoffSuccess);
+        }
+        else
+        {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void createUserFolder(){
+        // Test private method using reflection
+        Class[] parameters = new Class[1];
+        parameters[0] = String.class;
+
+        try
+        {
+            Method createUserFolderMethod = ftpServer.getClass().getDeclaredMethod("createUserFolder", parameters);
+            createUserFolderMethod.setAccessible(true);
+            boolean result = (boolean)createUserFolderMethod.invoke(ftpServer, "David");
+
+            Assert.assertTrue(result);
+        }
+        catch (Exception ex)
+        {
+            Assert.fail();
+        }
+    }
+}
